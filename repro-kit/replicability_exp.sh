95b5e5c0a5df40e8bd0c186471dab2d1dce3254f
Juan Manuel Rodriguez <juanmanuel.rodriguez@isistan.unicen.edu.ar>
2023-12-16 21:32:57 +0100

    Update scripts

--- repro-kit/replicability_exp.sh
+++ repro-kit/replicability_exp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 python experiment.py -z coco5k -s clip -m ViT-L/14@336px -e coco5k -r hit_rate@1,hit_rate@5,hit_rate@10 --header
+python experiment.py -z coco5k -s clip -m ViT-L/14 -e coco5k -r hit_rate@1,hit_rate@5,hit_rate@10 
+python experiment.py -z coco5k -s stclip -m ViT-L/14 -e coco5k -r hit_rate@1,hit_rate@5,hit_rate@10 
 python experiment.py -z coco5k -s bliprr -m coco-large -e coco5k -r hit_rate@1,hit_rate@5,hit_rate@10
 python experiment.py -z coco5k -s blip2rr -m coco -e coco5k -r hit_rate@1,hit_rate@5,hit_rate@10
 python experiment.py -z coco5k -s sgraf -e coco5k -r hit_rate@1,hit_rate@5,hit_rate@10
